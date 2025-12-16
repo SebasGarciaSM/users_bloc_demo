824a135244a66e9913b7f4784eda3218cf5ae3f5
SebasGarciaSM <75601981+SebasGarciaSM@users.noreply.github.com>
2025-12-16 18:11:49 -0500

    refactor: integrate GoRouter for navigation and add UserDetailsPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:users_bloc_demo/core/injection_container.dart';
 import 'package:users_bloc_demo/presentation/cubit/users_cubit.dart';
 import 'package:users_bloc_demo/presentation/home_page.dart';
+import 'package:users_bloc_demo/presentation/user_details_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await setupLocator();
 
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
-  // This widget is the root of your application.
+  final _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) {
+          return BlocProvider(
+            create: (context) => sl<UsersCubit>()..loadUsers(),
+            child: HomePage(),
+          );
+        },
+      ),
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: BlocProvider(
-        create: (_) => sl<UsersCubit>()..loadUsers(),
-        child: const HomePage(),
-      ),
+      routerConfig: _router,
+
+      // home: BlocProvider(
+      //   create: (_) => sl<UsersCubit>()..loadUsers(),
+      //   child: const UserDetailsPage(),
+      // ),
     );
   }
 }
---
+++ lib/presentation/user_details_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UserDetailsPage extends StatelessWidget {
+  const UserDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Details'),
+      ),
+      body: const Center(
+        child: Text('User Details Page'),
+      ),
+    );
+  }
+}
